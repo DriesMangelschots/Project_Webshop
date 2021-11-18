64bfc37dc6121099f14886339ec98fd43b035a98
Dries <r0590700@student.thomasmore.be>
2021-11-18 14:56:59 +0100

    Making Models from ERD

---
+++ Project_Webshop/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Webshop.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Password { get; set; }
+
+        // salt search how this works
+        // public string DeliveryAddress { get; set; }
+        public Newsletter Newsletter;
+        public ICollection<Order>Orders { get; set; }
+
+    }
+}
---
+++ Project_Webshop/Models/Newsletter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Webshop.Models
+{
+    public class Newsletter
+    {
+        public int NewsletterID { get; set; }
+        public Customer Customer { get; set; }
+
+       
+    }
+}
