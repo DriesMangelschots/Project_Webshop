c6411e14c490dca79a343690fdc4ee7850f9abaa
Dries <r0590700@student.thomasmore.be>
2021-12-08 11:44:29 +0100

    Setting up db

---
+++ Project_Webshop/Data/ProjectWebshopContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Webshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Webshop.Data
+{
+    public class ProjectWebshopContext: DbContext
+    {
+    
+            public ProjectWebshopContext(DbContextOptions<ProjectWebshopContext> options)
+                : base(options)
+            {
+
+            }
+            public DbSet<Customer> Customers { get; set; }
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<Orderline> Orderlines { get; set; }
+            public DbSet<Product> Products { get; set; }
+            public DbSet<Newsletter> Newsletters { get; set; }
+
+            //database
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Customer>().ToTable("Customer");
+                modelBuilder.Entity<Customer>().ToTable("Order");
+                modelBuilder.Entity<Customer>().ToTable("Orderline");
+                modelBuilder.Entity<Customer>().ToTable("Product");
+                modelBuilder.Entity<Customer>().ToTable("Newsletter");
+
+
+
+
+
+            }
+
+
+        
+    }
+}
--- Project_Webshop/Startup.cs
+++ Project_Webshop/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project_Webshop.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace Project_Webshop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // add context with DI + using sql server 
+            services.AddDbContext<ProjectWebshopContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("LocalDBConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
